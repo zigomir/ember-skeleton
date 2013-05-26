518811c6b86b6acab0031af7ffd254831a453ea3
zigomir <zigomir@gmail.com>
2013-05-26 13:06:32 +0200

    simple todo example

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,3 +3,21 @@ var App = Ember.Application.create();
 App.Router.map(function(){
   this.route('about');
 });
+
+App.Todo = Ember.Object.extend({});
+App.Todo.reopen({
+  isDoneChanged: function(todo) {
+    var isDone = todo.get('isDone') ? 'done.' : 'not done.';
+    console.log(todo.get('title') + ' is now ' + isDone);
+  }.observes('isDone')
+});
+
+var todo1 = App.Todo.create({title: 'First', isDone: false});
+var todo2 = App.Todo.create({title: 'Second', isDone: false});
+var todo3 = App.Todo.create({title: 'Third', isDone: false});
+
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return [todo1, todo2, todo3];
+  }
+});
